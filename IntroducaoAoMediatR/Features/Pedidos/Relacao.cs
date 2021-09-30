4fe2c1754a9125e116b7e321faf58f09c677de1f
Wedson Quintanilha da Silva <wedson.silva@basis.com.br>
2021-09-30 15:12:14 -0300

    Adicionar código fonte

---
+++ IntroducaoAoMediatR/Features/Pedidos/Relacao.cs
@@ -0,0 +1,24 @@
+﻿using IntroducaoAoMediatR.Models;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IntroducaoAoMediatR.Features
+{
+    public class Relacao : IRequest<IEnumerable<Pedido>> { }
+
+    public class RelacaoHandler : IAsyncRequestHandler<Relacao, IEnumerable<Pedido>>
+    {
+        private readonly IRepositorioDePedido repositorioDePedidos;
+
+        public RelacaoHandler(IRepositorioDePedido repositorioDePedidos)
+        {
+            this.repositorioDePedidos = repositorioDePedidos;
+        }
+
+        public async Task<IEnumerable<Pedido>> Handle(Relacao message)
+        {
+            return await repositorioDePedidos.BuscarUltimosPedidos();
+        }
+    }
+}
